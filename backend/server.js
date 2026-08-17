2e8b14cdf634a4c56a4ae676dbf854a68377afbb
Umer70237 <umerrao0789@gmail.com>
2026-08-17 10:45:56 +0300

    feat: add JWT authentication system

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import morgan from "morgan";
 
 import { connectDB } from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/error.middleware.js";
+import authRoutes from "./routes/auth.routes.js"
 
 const app = express();
 
@@ -23,6 +24,8 @@ if (process.env.NODE_ENV !== "production") app.use(morgan("dev"));
 app.get("/api/health", (req, res) =>
   res.json({ success: true, status: "ok", service: "TTP CRM API" })
 );
+
+app.use("/api/auth", authRoutes);
 
 
 /* ────────────────────────────── Error handling (last) ────────────────────────────── */
