8381139aedfcc8e105f78cd63851b8c2844288fa
Umer70237 <umerrao0789@gmail.com>
2026-09-08 08:33:02 +0300

    feat(analytics): add getOverview controller and analytics route

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,7 @@ import contactRoutes from "./routes/contact.routes.js";
 import noteRoutes from "./routes/note.routes.js";
 import taskRoutes from "./routes/task.routes.js";
 import aiRoutes from "./routes/ai.routes.js";
+import analyticsRoutes from "./routes/analytics.routes.js"
 
 const app = express();
 
@@ -36,6 +37,7 @@ app.use("/api/contacts", contactRoutes);
 app.use("/api/notes", noteRoutes);
 app.use("/api/tasks", taskRoutes);
 app.use("/api/ai", aiRoutes);
+app.use("/api/analytics", analyticsRoutes);
 
 
 /* ────────────────────────────── Error handling (last) ────────────────────────────── */
