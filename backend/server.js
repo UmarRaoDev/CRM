92bcabb07e2947fab16c9883fa8208624fd167ae
Umer70237 <umerrao0789@gmail.com>
2026-09-09 17:33:21 +0300

    allowed all origin request

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const app = express();
 /* ────────────────────────────── Middleware ────────────────────────────── */
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: true,
     credentials: true,
   })
 );
